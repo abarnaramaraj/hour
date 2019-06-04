1bb1829732fe537ac788ee48c3e5c98919b0dee8
abarnaramaraj <47477481+abarnaramaraj@users.noreply.github.com>
2019-06-04 19:00:55 +0530

    Create hour.java

---
+++ hour.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+public class Float
+{
+public static void main(String[] args)
+{
+float a,b,c;
+System.out.println("enter the two float");
+Scanner sc=new Scanner(System.in);
+a=sc.nextFloat;
+b=sc.nextFloat;
+c=a-b;
+System.out.println("subtract value"+c);
+}
+}
